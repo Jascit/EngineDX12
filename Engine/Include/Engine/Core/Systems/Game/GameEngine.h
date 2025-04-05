a9c8c6776e96d8810f9c811c8622c162caa8401a
Andrii <andrijm3@gmail.com>
2025-04-05 23:53:05 +0200

    .

--- Engine/Include/Engine/Core/Systems/Game/GameEngine.h
+++ Engine/Include/Engine/Core/Systems/Game/GameEngine.h
@@ -1,1 +1,24 @@
 #pragma once
+class GameEngine {
+public:
+  void initialize();
+  void update();
+  void render();
+  void shutdown();
+private:
+  //TODO: systems
+  //RenderingSystem renderingService;
+  //PhysicsSystem physicsService;
+  //InputSystem inputService;
+  //AudioSystem audioService;
+  //NetworkSystem networkService;
+public:
+  GameEngine(const GameEngine&) = delete;
+  GameEngine& operator=(const GameEngine&) = delete;
+  static GameEngine& Get() {
+    static GameEngine instance;
+    return instance;
+  }
+private:
+  GameEngine() = default;
+};
